d61a5276c8f754f2a5d63726f03e00aad8b46c50
Szymon Nitecki <szymon.nitecki.ready4s.pl@gmail.com>
2017-04-02 00:59:27 +0200

    update

--- www/js/MainScreen.js
+++ www/js/MainScreen.js
@@ -1,11 +1,38 @@
-function initializeMap() {
-  var myOptions = {
-    center: new google.maps.LatLng(-34.397, 150.644),
-    zoom: 8,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  var map = new google.maps.Map(document.getElementById("map-canvas"),
-    myOptions);
-}
+    var map;
+    document.addEventListener("deviceready", function() {
+      var div = document.getElementById("map-canvas");
 
-$(document).on("pagecreate", initializeMap);
+      // Initialize the map view
+      map = plugin.google.maps.Map.getMap(div);
+
+      // Wait until the map is ready status.
+      map.addEventListener(plugin.google.maps.event.MAP_READY, onMapReady);
+
+    // onSuccess Callback 
+    // This method accepts a Position object, which contains the 
+    // current GPS coordinates 
+    // 
+    var onSuccess = function(position) {
+        alert('Latitude: '          + position.coords.latitude          + '\n' +
+              'Longitude: '         + position.coords.longitude         + '\n' +
+              'Altitude: '          + position.coords.altitude          + '\n' +
+              'Accuracy: '          + position.coords.accuracy          + '\n' +
+              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+              'Heading: '           + position.coords.heading           + '\n' +
+              'Speed: '             + position.coords.speed             + '\n' +
+              'Timestamp: '         + position.timestamp                + '\n');
+    };
+ 
+    // onError Callback receives a PositionError object 
+    // 
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+ 
+    navigator.geolocation.watchPosition(onSuccess, onError);
+    }, false);
+
+    function onMapReady() {
+    	map.showDialog();
+    }
