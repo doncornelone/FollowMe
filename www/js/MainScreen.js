9ecf4bb3050074d0c8bcd9a9a323b3503c4931c6
Szymon Nitecki <szymon.nitecki.ready4s.pl@gmail.com>
2017-04-02 00:46:37 +0200

    update

--- www/js/MainScreen.js
+++ www/js/MainScreen.js
@@ -13,14 +13,14 @@
     // current GPS coordinates 
     // 
     var onSuccess = function(position) {
-        alert('Latitude: '          + position.coords.latitude          + '\n' +
-              'Longitude: '         + position.coords.longitude         + '\n' +
-              'Altitude: '          + position.coords.altitude          + '\n' +
-              'Accuracy: '          + position.coords.accuracy          + '\n' +
-              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-              'Heading: '           + position.coords.heading           + '\n' +
-              'Speed: '             + position.coords.speed             + '\n' +
-              'Timestamp: '         + position.timestamp                + '\n');
+        // alert('Latitude: '          + position.coords.latitude          + '\n' +
+        //       'Longitude: '         + position.coords.longitude         + '\n' +
+        //       'Altitude: '          + position.coords.altitude          + '\n' +
+        //       'Accuracy: '          + position.coords.accuracy          + '\n' +
+        //       'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+        //       'Heading: '           + position.coords.heading           + '\n' +
+        //       'Speed: '             + position.coords.speed             + '\n' +
+        //       'Timestamp: '         + position.timestamp                + '\n');
     };
  
     // onError Callback receives a PositionError object 
@@ -34,5 +34,5 @@
     }, false);
 
     function onMapReady() {
-    	map.showDialog();
+    	//map.showDialog();
     }
