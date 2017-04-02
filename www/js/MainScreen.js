2d50ed963b24af8d6b3361ac2891e1d8586520e1
Szymon Nitecki <szymon.nitecki.ready4s.pl@gmail.com>
2017-04-02 12:02:46 +0200

    update

--- www/js/MainScreen.js
+++ www/js/MainScreen.js
@@ -11,20 +11,20 @@ document.addEventListener("deviceready", function() {
 
 function onMapReady() {
   $.mobile.loadingMessage = false;
-  navigator.geolocation.getCurrentPosition(onSuccess, onError, { timeout: 30000 });
-  function onSuccess(position) {
-    var lat=position.coords.latitude;
-    var lang=position.coords.longitude;
+//   navigator.geolocation.getCurrentPosition(onSuccess, onError, { timeout: 30000 });
+//   function onSuccess(position) {
+//     var lat=position.coords.latitude;
+//     var lang=position.coords.longitude;
 
-//Google Maps
-var myLatlng = new google.maps.LatLng(lat,lang);
-var mapOptions = {zoom: 4,center: myLatlng}
-var marker = new google.maps.Marker({position: myLatlng,map: map});
-}
-function onError(error) {
-  alert('code: ' + error.code + '\n' +
-    'message: ' + error.message + '\n');
-}
+// //Google Maps
+// var myLatlng = new google.maps.LatLng(lat,lang);
+// var mapOptions = {zoom: 4,center: myLatlng}
+// var marker = new google.maps.Marker({position: myLatlng,map: map});
+// }
+// function onError(error) {
+//   alert('code: ' + error.code + '\n' +
+//     'message: ' + error.message + '\n');
+// }
 }
 
 $(document).on( "pagecontainershow", function(){
