39c3356d74008c05c001d15a681e41525a5d290d
Roksana Butryn <r.butryn@op.pl>
2017-04-09 04:49:21 +0200

    login

--- www/js/MainScreen.js
+++ www/js/MainScreen.js
@@ -1,81 +1,69 @@
-    var map;
-
+// GLOBAL
     function onAppLoad() {
       console.log("onAppLoad")
-          var config = {
-    apiKey: "AIzaSyD--adRZnZ2o4FeQEWZ-5QhdDumE_moJM4",
-    authDomain: "bai-app-e48cd.firebaseapp.com",
-    databaseURL: "https://bai-app-e48cd.firebaseio.com",
-    projectId: "bai-app-e48cd",
-    storageBucket: "bai-app-e48cd.appspot.com",
-    messagingSenderId: "1097771959425"
-  };
-  firebase.initializeApp(config);
-        }
-    document.addEventListener("deviceready", function() {
+      var config = {
+        apiKey: "AIzaSyD--adRZnZ2o4FeQEWZ-5QhdDumE_moJM4",
+        authDomain: "bai-app-e48cd.firebaseapp.com",
+        databaseURL: "https://bai-app-e48cd.firebaseio.com",
+        projectId: "bai-app-e48cd",
+        storageBucket: "bai-app-e48cd.appspot.com",
+        messagingSenderId: "1097771959425"
+      };
+      firebase.initializeApp(config);
+    }
+
+//MAIN SCREEN
+    var map;
+    $(document).on("pageshow", "#map-page", function( event ) {
+         ScaleContentToDevice();        
       var div = document.getElementById("map_canvas");
 
       // Initialize the map view
       map = plugin.google.maps.Map.getMap(div);
+      map.addEventListener(plugin.google.maps.event.MAP_READY, onMapReady);
 
-      // Wait until the map is ready status.
-       map.addEventListener(plugin.google.maps.event.MAP_READY, onMapReady);
-     }, false);
- 
-     function onMapReady() {
-       $.mobile.loadingMessage = false;
-           function onSuccess(position) {
-            map.moveCamera({
-  'target': new plugin.google.maps.LatLng(position.coords.latitude, position.coords.longitude),
-  'zoom': 18,
-}, function() {
-  console.log("Camera position changed.");
-});
+    })
+
+    function onMapReady() {
+     $.mobile.loadingMessage = false;
+
+     function onSuccess(position) {
+      map.moveCamera({
+        'target': new plugin.google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+        'zoom': 18,
+      }, function() {
+      });
     }
 
-    // onError Callback receives a PositionError object
-    //
     function onError(error) {
-        alert('code: '    + error.code    + '\n' +
-              'message: ' + error.message + '\n');
+      alert('code: '    + error.code    + '\n' +
+        'message: ' + error.message + '\n');
     }
-
-    // Options: throw an error if no update is received every 30 seconds.
-    //
     var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000 });
-     }
- 
-     $(document).on( "pagecontainershow", function(){
-       ScaleContentToDevice();        
-     });
+  }
 
-    $(document).on( "pageshow", function( event, ui ) {
-      console.log(event.target.id);
-    });
- 
-     $(window).on("resize orientationchange", function(){
-       ScaleContentToDevice();
-     });
- 
-     function ScaleContentToDevice(){
-       scroll(0, 0);
-       var content = $.mobile.getScreenHeight() - $(".ui-header").outerHeight() - $(".ui-footer").outerHeight() - $(".ui-content").outerHeight() + $(".ui-content").height();
-       $(".ui-content").height(content);
-     }
- 
-     $( document ).on( "mobileinit", function() {
-       $.mobile.loader.prototype.options.disabled = true;
-     });
+ //  $(document).on( "pagecontainershow", function(){
+ //   ScaleContentToDevice();        
+ // });
 
-     function onLoad() {
-      console.log("here");
-  var database = firebase.database();
-  firebase.database().ref('users/' + "userId").set({
-    username: "name",
-    email: "email",
-    profile_picture : "imageUrl"
-  });
-     }
+ //  $(document).on( "pageshow", function( event, ui ) {
+ //    console.log(event.target.id);
+ //    ScaleContentToDevice();
+ //  });
+
+ //  $(window).on("resize orientationchange", function(){
+ //   ScaleContentToDevice();
+ // });
+
+  function ScaleContentToDevice(){
+   scroll(0, 0);
+   var content = $.mobile.getScreenHeight() - $(".ui-header").outerHeight() - $(".ui-footer").outerHeight() - $(".ui-content").outerHeight() + $(".ui-content").height();
+   $(".ui-content").height(content);
+ }
+ 
+ $( document ).on( "mobileinit", function() {
+   $.mobile.loader.prototype.options.disabled = true;
+ });
 
 
 // REGISTRATION
@@ -103,9 +91,9 @@ function registerUser() {
 function loginUser() {
   var email = $('#login-txt-email').val();
   var password = $('#login-txt-password').val();
-    firebase.auth().signInWithEmailAndPassword(email, password).then(function(user){
-        $.mobile.changePage("#map-page");
-    },function(error) {
+  firebase.auth().signInWithEmailAndPassword(email, password).then(function(user){
+    $.mobile.changePage("#map-page");
+  },function(error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
@@ -117,9 +105,9 @@ function resetPassword() {
   var auth = firebase.auth();
   var email = $('#login-txt-email').val();
 
-auth.sendPasswordResetEmail(email).then(function() {
+  auth.sendPasswordResetEmail(email).then(function() {
     navigator.notification.alert('We sent you an email with instructions on how to reset your password.');
-}, function(error) {
+  }, function(error) {
     navigator.notification.alert(error.message);
-});
+  });
 }
