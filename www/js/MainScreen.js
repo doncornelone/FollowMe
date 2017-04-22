cdd3f1ed9860e13e623bad0927f4b3b58b319418
Szymon Nitecki <szymon.nitecki.ready4s.pl@gmail.com>
2017-04-22 02:56:28 +0200

    almost final version

--- www/js/MainScreen.js
+++ www/js/MainScreen.js
@@ -15,6 +15,17 @@ function onAppLoad() {
     messagingSenderId: "1097771959425"
   };
   firebase.initializeApp(config);
+  ScaleContentToDevice();
+
+
+}
+
+function showLoader(message) {
+  $.mobile.loading( 'show', {text : message, textVisible : true});
+}
+
+function hideLoader() {
+  $.mobile.loading( 'hide');
 }
 
 //MAIN SCREEN
@@ -22,7 +33,6 @@ var map;
 var currentPositionMarker;
 var sharedLocationMarkerArray = [];
 function initMap() {
-  ScaleContentToDevice();
   var latlng = new google.maps.LatLng(50, 20);
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 10,
@@ -34,6 +44,7 @@ function initMap() {
 function initMapPage() {
   var firstLocation = true;
   var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000 });
+    showLoader("Waiting for location...")
   firebase.database().ref('location-shares').once('value').then(function(snapshot) {
     var locationShares = snapshot.val();
     snapshot.forEach(function (snapshot){
@@ -88,6 +99,7 @@ function initMapPage() {
   });
 
   function onSuccess(position) {
+    hideLoader();
     var location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
     if (firstLocation){
       firstLocation = false;
@@ -115,8 +127,7 @@ function initMapPage() {
   }
 
   function onError(error) {
-    alert('code: '    + error.code    + '\n' +
-      'message: ' + error.message + '\n');
+    hideLoader();
   }
 
   function getCurrentUserInfoWindowContent(avat, name) {
@@ -152,21 +163,26 @@ function onMenuClick(name) {
     case 'map':
     $('#replacement-target').html( $('#map-content').html() ); 
     initMapPage();
+    $('#title-text').html("Map");
     break;
     case 'share':
     $('#replacement-target').html( $('#share').html() ); 
     initShare();
+    $('#title-text').html("Share location");
     break;
     case 'people':
     $('#replacement-target').html( $('#people').html() ); 
     initPeople();
+    $('#title-text').html("Chat");
     break;
     case 'history':
     $('#replacement-target').html( $('#history').html() ); 
     historyClick(0);
+    $('#title-text').html("History");
     break;
     case 'settings':
     $('#replacement-target').html( $('#settings').html() ); 
+    $('#title-text').html("Profile");
     break;
   }
 }
@@ -176,12 +192,28 @@ var historyType = 0
 function historyClick(argument) {
   console.log("history click");
   historyType = argument;
+  if (historyType == 0){
+    $("#btn-my-shares").addClass("ui-btn-active");
+    $("#btn-my-shares").removeClass("ui-btn-b");
+
+    $("#btn-shared-to-me").addClass("ui-bar-b")
+    $("#btn-shared-to-me").removeClass("ui-btn-active")
+  } else {
+    $("#btn-shared-to-me").addClass("ui-btn-active");
+    $("#btn-shared-to-me").removeClass("ui-btn-b");
+
+    $("#btn-my-shares").addClass("ui-bar-b")
+    $("#btn-my-shares").removeClass("ui-btn-active")
+  }
   $("#shares-history").empty();
   initHistory();
 }
 
 function initHistory(){
+  $('#title-text').html("History");
+    showLoader("Fetching data...");
   firebase.database().ref('location-shares').once('value').then(function(snap) {
+    hideLoader();
     console.log(snap.val())
     snap.forEach(function (snapshot){
       var authorId = snapshot.child("authorId").val();
@@ -211,14 +243,19 @@ function initHistory(){
         }
       });
     });
+    if ($("#shares-history").html() == ""){
+      $("#shares-history").html("<p style=\"margin-top:45px; text-align:center;\">The list is empty. Choose 'Share location' in menu to begin.</p>")
+    }
   });
 }
 
 //PEOPLE
 var chosenMessageUser;
 function initPeople() {
+  showLoader("Fetching data...");
   var database = firebase.database();
   firebase.database().ref('users').once('value').then(function(snapshot) {
+    hideLoader();
     console.log(snapshot.val());
     var users = snapshot.val();
     snapshot.forEach(function (snapshot){
@@ -227,25 +264,26 @@ function initPeople() {
       var name = snapshot.child("username").val();
       var avat = snapshot.child("avatar").val();
       if (email != null && key != userId){
-        $("#people-list").append("<a id=\"startChat\" href=\"#chat-page\" download=\"" + key + "\"class=\"ui-btn ui-btn-b ui-corner-all mc-top-margin-1-5\"><table><tr><td valign=\"center\"><img src=\"" + avat + "\" style=\"height:40px; width:40px;\"></td><td valign=\"center\">" + name + "</td></tr></table></a>");
+        $("#people-list").append("<a id=\"startChat\" href=\"#chat-page\" download=\"" + key + "\"class=\"ui-btn ui-btn-b ui-corner-all mc-top-margin-1-5\"><table style=\"pointer-events: none; cursor: default;\"><tr><td valign=\"center\"><img src=\"" + avat + "\" style=\"height:40px; width:40px; pointer-events: none; cursor: default;\"></td><td style=\"pointer-events: none; cursor: default;\" valign=\"center\">" + name + "</td></tr></table></a>");
       }
     });
   });
 }
 
 $(document).on('click', "#startChat" , function(argument) {
-  console.log("sendmessage");
   chosenMessageUser = argument.target.download;
+  console.log(argument);
   $.mobile.changePage("#chat-page");
 
 });
 
 //CHAT PAGE
 $(document).on("pageshow","#chat-page",function(){
-  scaleNormal()
+  showLoader("Fetching data...");
   var starCountRef = firebase.database().ref('messages');
   starCountRef.orderByChild("time");
   starCountRef.on('value', function(snap) {
+    hideLoader();
     $("messages-list").empty();
     console.log($("#messages-list").empty())
     snap.forEach(function(s){
@@ -254,18 +292,25 @@ $(document).on("pageshow","#chat-page",function(){
       console.log(authorId + " " + recipientId + " " + userId)
       var currentUserMessage = "";
       var otherUserMessage = "";
-      if (authorId == userId){
+      if (authorId == userId && recipientId == chosenMessageUser){
         currentUserMessage = '<div class="ui-body ui-body-a ui-corner-all">'+ s.child("content").val() +'</div>';
-      } else if (recipientId == userId){
+      } else if (recipientId == userId && authorId == chosenMessageUser){
         otherUserMessage = '<div class="ui-body ui-body-a ui-corner-all">'+ s.child("content").val() +'</div>';
       }
+      if (currentUserMessage != "" && otherUserMessage != ""){
       $("#messages-list").append('<div class="ui-block-a">' +
         otherUserMessage +
         '</div>'+
         '<div class="ui-block-b">'+
         currentUserMessage +
         '</div>')
+      window.scrollTo(0,document.body.scrollHeight);
+    }
     })
+    console.log($("#messages-list").html());
+    if ($("#messages-list").html() == ""){
+      $("#messages-list").html("<p style=\"margin-top:45px; text-align:center;\">You have no message with this user - write something</p>")
+    }
   })
 });
 
@@ -283,16 +328,11 @@ function sendMessage(){
   });
 }
 
+var initialHeight;
 function ScaleContentToDevice(){
  scroll(0, 0);
- var content = $.mobile.getScreenHeight() - $(".ui-header").outerHeight() - $(".ui-footer").outerHeight() - $(".ui-content").outerHeight() + $(".ui-content").height();
- $(".ui-content").height(content);
-}
-
-function scaleNormal(){
- scroll(0, 0);
- var content = $.mobile.getScreenHeight() - $(".ui-header").outerHeight() - $(".ui-footer").outerHeight() - $(".ui-content").outerHeight() + $(".ui-content").height() ;
- $(".ui-content").height(content); 
+ var content = $.mobile.getScreenHeight() - $(".ui-content").outerHeight() + $(".ui-content").height() - 16;
+ $("#replacement-target").height(content);
 }
 
 $( document ).on( "mobileinit", function() {
@@ -304,7 +344,9 @@ $( document ).on( "mobileinit", function() {
 
  function initShare() {
   var database = firebase.database();
+      showLoader("Fetching data...");
   firebase.database().ref('users').once('value').then(function(snapshot) {
+    hideLoader();
     console.log(snapshot.val());
     var users = snapshot.val();
     snapshot.forEach(function (snapshot){
@@ -313,13 +355,34 @@ $( document ).on( "mobileinit", function() {
       var name = snapshot.child("username").val();
       var avat = snapshot.child("avatar").val();
       if (email != null && key != userId){
-        $("#location-recipient-list").append("<a id=\"locationShareButton\" download=\"" + key + "\"class=\"ui-btn ui-btn-b ui-corner-all mc-top-margin-1-5\"><table><tr><td valign=\"center\"><img src=\"" + avat + "\" style=\"height:40px; width:40px;\"></td><td valign=\"center\">" + name + "</td></tr></table></a>");
+        $("#location-recipient-list").append("<a id=\"locationShareButton\" download=\"" + key + "\"class=\"ui-btn ui-btn-b ui-corner-all mc-top-margin-1-5\"><table style=\"pointer-events: none; cursor: default;\"><tr><td valign=\"center\"><img src=\"" + avat + "\" style=\"height:40px; width:40px;\"></td><td valign=\"center\">" + name + "</td></tr></table></a>");
       }
     });
   });
 }
 
 function durationClick(duration) {
+    $("#btn-share-5").addClass("ui-btn-a");
+    $("#btn-share-15").addClass("ui-btn-a");
+    $("#btn-share-30").addClass("ui-btn-a");
+    $("#btn-share-5").removeClass("ui-btn-active");
+    $("#btn-share-15").removeClass("ui-btn-active");
+    $("#btn-share-30").removeClass("ui-btn-active");
+    switch (duration){
+      case 5:
+      $("#btn-share-5").removeClass("ui-btn-a");
+      $("#btn-share-5").addClass("ui-btn-active");
+      break;
+      case 15:
+      $("#btn-share-15").removeClass("ui-btn-a");
+      $("#btn-share-15").addClass("ui-btn-active");
+      break;
+      case 30:    
+      $("#btn-share-30").removeClass("ui-btn-a");
+      $("#btn-share-30").addClass("ui-btn-active");
+      break;
+    }
+
   locationShareDuration = duration;
 }
 
@@ -334,7 +397,7 @@ $(document).on('click', "#locationShareButton" , function(argument) {
     recipientId: key,
     expirationTime: expirationTime
   });
-  navigator.notification.alert('You succesfully share location!', function (){
+  navigator.notification.alert('You successfully share location!', function (){
     $('#replacement-target').html( $('#history').html() ); 
   });
 });
@@ -354,8 +417,10 @@ function registerUser() {
   if (password != passwordConfirm){
     navigator.notification.alert('Given passwords are different!');
   } else {
+        showLoader("Registering...");
     firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user){
       userId = user.uid;
+      hideLoader();
       navigator.notification.alert('You are now registered!', function () {
         var database = firebase.database();
         firebase.database().ref('users/' + userId).set({
@@ -387,9 +452,12 @@ function chosenAvatar(number) {
 
 // LOGIN
 function loginUser() {
+  showLoader("Signing in...")
   var email = $('#login-txt-email').val();
   var password = $('#login-txt-password').val();
   firebase.auth().signInWithEmailAndPassword(email, password).then(function(user){
+      $('#login-txt-email').val("")
+  $('#login-txt-password').val("")
     userEmail = email;
     userId = user.uid;
     var database = firebase.database();
@@ -397,10 +465,14 @@ function loginUser() {
       var user = snapshot.val();
       avatar = user.avatar;
       username = user.username;
+      $("#profile-username-txt").html("<b>" + username + "</b>");
+      $("#profile-email-txt").html(userEmail);
+      $("#profile-user-img").attr("src", avatar);
         $('#replacement-target').html( $('#history').html() ); 
+        hideLoader();
   historyClick(0);
     });
-    $.mobile.changePage("#map-page");
+    $.mobile.changePage("#main-page");
   },function(error) {
     // Handle Errors here.
     var errorCode = error.code;
@@ -415,10 +487,13 @@ function resetPassword() {
 }
 
 function resetPass(email) {
+    showLoader("Resetting password...")
   var auth = firebase.auth();
   auth.sendPasswordResetEmail(email).then(function() {
+    hideLoader();
     navigator.notification.alert('We sent you an email with instructions on how to reset your password.');
   }, function(error) {
+    hideLoader();
     navigator.notification.alert(error.message);
   });}
 
